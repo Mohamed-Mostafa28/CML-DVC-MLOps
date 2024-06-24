c68d89c514bd90a9e52aa5ebd340c100663bf243
Mohamed_Mostafa <mohamedmostafa012345012345@gmail.com>
2024-06-24 11:59:19 +0300

    dvc commit

--- exp_run.py
+++ exp_run.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
+# sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
 from Utilities import load_params 
 from training import ModelTrainer
 from preprocessing import DataPreprocessor
