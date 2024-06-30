192837d0a74f124c3663a8d7ccdafaa3d8cc44a5
Mohamed_Mostafa <mohamedmostafa012345012345@gmail.com>
2024-06-30 12:42:25 +0300

    add

--- src/deplib/run/exp_run.py
+++ src/deplib/run/exp_run.py
@@ -15,7 +15,7 @@ print("-----------------------------")
 
 def main():
  
-    print("tttttttttttttttttttttttttttttttttttttt")
+    print("ttttttttttttttttttttttttttttt")
     with open(f"../../../params.yml", 'r', encoding="utf-8") as file:
     # with open(r"../../../../params.yml", 'r', encoding="utf-8") as file:
         params = yaml.safe_load(file)
